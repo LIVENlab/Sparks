d4ba48253cc58da7d5d15532a6476291a5db8b27
Alexander <altz7@hotmail.com>
2023-11-04 16:07:38 +0100

    EuroCalliope data

---
+++ sentinel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from ProspectBackground.util.update_experiment import Prospect
+
+enbios_mod = Prospect(r'C:\Users\altz7\PycharmProjects\enbios__git\projects\data_sentinel\flow_out_sum.csv',
+                       r'C:\Users\altz7\PycharmProjects\enbios__git\projects\seed\MixUpdater\data\base_file_simplified_v2.xlsx',
+                       'Seeds_exp4', 'db_experiments')
+
+enbios_mod.preprocess(subregions=False)
+enbios_mod.data_for_ENBIOS(smaller_vers=1)
+
+
+df=pd.read_csv(r'C:\Users\altz7\PycharmProjects\enbios__git\projects\data_sentinel\flow_out_sum.csv',delimiter=',')
