5834a7ec43e701c9ec306d871bbeb21286861c8a
LexPascal <1496051@uab.cat>
2024-05-21 18:35:38 +0200

    fix base testing
    Signed-off-by: ramin-server <diskordier@gmail.com>

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
-from Sparks.util.update_experiment import SoftLink
+from Sparks.util.base import SoftLink
 
 
 enbios_mod = SoftLink(r'C:\Users\Administrator\Downloads\flow_out_sum (1).csv',
                       r'C:\Users\Administrator\Documents\Alex\BASEFILE\basefile_off_heat.xlsx',
-                       'seeds_REPORT_V1', 'ecoinvent')
+                       'github', 'ecoinvent')
 pass
 enbios_mod.preprocess(subregions=False)
 enbios_mod.data_for_ENBIOS(path_save=r'data_enbios_paper/test.json')
