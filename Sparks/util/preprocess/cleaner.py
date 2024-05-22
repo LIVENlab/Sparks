1119728ed15cb850b901e5322b80672677ac24e0
LexPascal <1496051@uab.cat>
2024-05-22 09:42:53 +0200

    Improve readability
    Signed-off-by: ramin-server <diskordier@gmail.com>

--- Sparks/util/preprocess/cleaner.py
+++ Sparks/util/preprocess/cleaner.py
@@ -25,13 +25,11 @@ class BaseFileActivity:
     carrier: str
     parent: str
     code:str
-    #activity: Optional['Activity'] = None
     factor: Union[int, float]
     alias: Optional[str] = None
     unit: Optional[str] = None
 
     def __post_init__(self):
-        pass
         self.alias = f"{self.name}_{self.carrier}"
         self.activity = self._load_activity(key=self.code)
 
@@ -51,10 +49,11 @@ class BaseFileActivity:
 
 
 
+
 class Cleaner:
-    """
-    Clean the input data and modify the units
-    """
+    """Clean the input data and modify the units"""
+
+
     def __init__(self,
                  caliope: str,
                  motherfile: str,
@@ -64,23 +63,15 @@ class Cleaner:
         self._raw_data = caliope
         self.mother_file = motherfile
         self.final_df = None
-        self.activity_conversion_dictionary = {}
         self.techs_region_not_included = []
 
 
     @staticmethod
-    def create_df() -> pd.DataFrame:
+    def create_template_df() -> pd.DataFrame:
         """
         Basic template for clean data
         """
-        columns = [
-            "spores",
-            "techs",
-            "locs",
-            "carriers",
-            "unit",
-            "flow_out_sum"
-        ]
+        columns = ["spores","techs","locs","carriers","unit","flow_out_sum"]
         return pd.DataFrame(columns=columns)
 
 
@@ -90,11 +81,10 @@ class Cleaner:
 
 
     def _input_checker(self):
-        data=self._load_data()
         """
         Check whether the input from Calliope follows the expected structure
-        data: pd.Dataframe
         """
+        data = self._load_data()
         expected_cols = {'spores',
                          'techs',
                          'locs',
@@ -102,31 +92,31 @@ class Cleaner:
                          'unit',
                          'flow_out_sum'}
         cols = set(data.columns) # Search for possible differences. Older versions of calliope produce "spore"
+
         if "spore" in cols:
             data.rename(columns={'spore': 'spores'}, inplace=True)
             cols = set(data.columns)
+
         if expected_cols != cols:
             raise KeyError(f"Columns {cols} do not match the expected columns: {expected_cols}")
 
-    def _manage_regions(self, loc)->str:
 
-        if isinstance(loc, tuple):
-            arg = loc[0]
-            region = arg.split('-')[0].split('_')[0]
-        # Special issue for the Portugal Analysis
+    def _get_region_from_location(self, loc: str) -> str:
+        """ Extract the region from the location string"""
         if loc == 'ESP-sink':
             return 'ESP'
-        else:
-            return loc.split('-')[0].split('_')[0]
-    def _group_df(self,
-                  df: pd.DataFrame)-> pd.DataFrame:
-        """ group input data by specified criteria"""
+        return loc.split('-')[0].split('_')[0]
 
-        gen_df = self.create_df()
+
+    def _group_data(self, df: pd.DataFrame)-> pd.DataFrame:
+        """ Group input data by specified criteria"""
+
+        grouped_df = self.create_template_df()
         scenarios = df['spores'].unique()
 
         if not self.subregions:
-            df['locs'] = df['locs'].apply(self._manage_regions)
+            df['locs'] = df['locs'].apply(self._get_region_from_location)
+
         for scenario in scenarios:
             df_sub = df[df['spores'] == scenario]
             df_sub = df_sub.groupby(['techs', 'locs', 'carriers']).agg({
@@ -134,25 +124,23 @@ class Cleaner:
                 "unit": "first",
                 "flow_out_sum": "sum"
             }).reset_index()
-            gen_df = pd.concat([gen_df, df_sub])
-        return gen_df
+            grouped_df = pd.concat([grouped_df, df_sub])
+
+        return grouped_df
 
 
     def _adapt_data(self)->pd.DataFrame:
-
         print('Adapting input data...')
         try:
             df=self._load_data()
             self._input_checker()
         except FileNotFoundError:
             raise FileNotFoundError(f'File {self._raw_data} does not exist. Please check it')
-
-        return self._group_df(df)
-
+        return self._group_data(df)
 
 
-    def filter_techs(self,
-                     df: pd.DataFrame)-> pd.DataFrame:
+
+    def filter_techs(self,df: pd.DataFrame)-> pd.DataFrame:
         """
         Filter the input data based on technologies defined in the basefile
         """
@@ -164,31 +152,25 @@ class Cleaner:
         self.basefile['alias_carrier']=self.basefile['Processor']+ '_' + self.basefile['@SimulationCarrier']
         excluded_techs = set(df_names['techs']) - set(self.basefile['Processor'])
         self.techs_region_not_included=excluded_techs
-
         df_names = df_names[~df_names['techs'].isin(excluded_techs)] # exclude the technologies
 
-        if len(excluded_techs)>0:
+        if excluded_techs:
             message=f'''\nThe following technologies, are present in the energy data but not in the Basefile: 
             \n{excluded_techs}
             \n Please,check the following items in order to avoid missing information'''
             warnings.warn(message,Warning)
-
         return df_names
 
+
     def preprocess_data(self)->pd.DataFrame:
-        """
-        Run data preprocessing steps
-        """
+        """Run data preprocessing steps"""
         self.final_df = self.filter_techs(self._adapt_data())
         return self.final_df
 
 
-
-
-
     def _extract_data(self)->List['BaseFileActivity']:
         base_activities=[]
-        for i,r in self.basefile.iterrows():
+        for _,r in self.basefile.iterrows():
             base_activities.append(
                 BaseFileActivity(
                     name=r['Processor'],
@@ -200,28 +182,24 @@ class Cleaner:
                 )
             )
 
-        self.base_activities=base_activities
-        pass
-        self.base_activities = [x for x in self.base_activities if x.unit is not None]
-
+        return  [activity for activity in base_activities if activity.unit is not None]
 
 
     def _adapt_units(self):
-        """
-        adapt the units (flow_out_sum * conversion factor)
-        """
-        self._extract_data()
-        pass
+        """adapt the units (flow_out_sum * conversion factor)"""
+        self.base_activities=self._extract_data()
+
         alias_to_factor = {x.alias: x.factor for x in self.base_activities}
-        unit_to_factor = {x.alias: x.unit for x in self.base_activities }
-        pass
+        unit_to_factor = {x.alias: x.unit for x in self.base_activities}
+
         self.final_df['new_vals'] = self.final_df['alias_carrier'].map(alias_to_factor) * self.final_df['flow_out_sum']
         self.final_df['new_units'] =self.final_df['alias_carrier'].map(unit_to_factor)
-        return self.edit_final_df(self.final_df)
+
+        return self._final_dataframe(self.final_df)
 
 
     @staticmethod
-    def edit_final_df(df):
+    def _final_dataframe(df):
         cols = ['spores',
                 'locs',
                 'techs',
@@ -236,6 +214,7 @@ class Cleaner:
 
 
     def adapt_units(self):
+        """Public method to adapt the units"""
         return self._adapt_units()
 
 
