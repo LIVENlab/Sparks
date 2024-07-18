663c5faf26bced55f99b0a2d56ddbbfb2f8efb36
Alexander <altz7@hotmail.com>
2024-07-18 09:39:36 +0200

    last update

--- demo_test.py
+++ demo_test.py
@@ -1,6 +1,5 @@
 from Sparks.util.base import SoftLink
 
-
 enbios_mod = SoftLink(r'C:\Users\Administrator\Downloads\flow_out_sum (1).csv',
                       r'C:\Users\Administrator\Documents\Alex\BASEFILE\basefile_off_heat.xlsx',
                        'github',
