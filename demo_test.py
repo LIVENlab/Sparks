61fff1aa98cfb8958ed064cbfe36fee0469d9c3f
Alexander <altz7@hotmail.com>
2025-02-14 15:26:51 +0100

    update for sup basefile testing

--- demo_test.py
+++ demo_test.py
@@ -2,7 +2,12 @@ from Sparks.util.base import SoftLink
 
 enbios_mod = SoftLink(r'C:\Users\altz7\PycharmProjects\ENBIOS4TIMES_\testing\data_test',
                     'Hydrogen_SEEDS')
+
+basefile = enbios_mod.sup_basefile()
 pass
-enbios_mod.sup_basefile()
-enbios_mod.preprocess()
-enbios_mod.data_for_ENBIOS(smaller_vers=False, path_save=r'test.json')
+
+
+
+
+#enbios_mod.preprocess()
+#enbios_mod.data_for_ENBIOS(smaller_vers=False, path_save=r'test.json')
